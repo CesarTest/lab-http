a9606f42840f756405da459c3935fd371ccfd870
root <root@Tuxedo.>
2026-06-07 10:04:25 +0200

    Remove Site

--- roles/cdg.grav/files/sites/home/themes/labs/labs.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace Grav\Theme;
-use Grav\Common\Theme;
-class Labs extends quark
-{
-}
-?>
--- roles/cdg.grav/files/sites/maas/themes/maas/maas.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace Grav\Theme;
-use Grav\Common\Theme;
-class maas extends Learn4
-{
-}
-?>
